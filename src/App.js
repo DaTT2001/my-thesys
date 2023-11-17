335574f490b869b472500e9b8645978663b4d4fb
Duong Cong Dat <104434271+DaTT2001@users.noreply.github.com>
2023-11-17 16:55:34 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./components/Button";
+import GasMeter from "./components/GasMeter";
+import HumidityMeter from "./components/HumidityMeter";
+import Temperature from "./components/Temperature";
+import "./App.css"
+import { API_TOKEN } from "./constants";
+import { useEffect, useState } from "react";
+import axios from "axios"
 
 function App() {
+  const [temp, setTemp] = useState(0)
+  const [humi, setHumi] = useState(0)
+  const [gas, setGas] = useState(0)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://blynk.cloud/external/api/get?token=${API_TOKEN}&v1`);
+        setTemp(response.data)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 1000);
+
+    // Hủy interval khi component bị unmount
+    return () => clearInterval(intervalId);
+  }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://blynk.cloud/external/api/get?token=${API_TOKEN}&v2`);
+        setHumi(response.data)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 1000);
+
+    // Hủy interval khi component bị unmount
+    return () => clearInterval(intervalId);
+  }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://blynk.cloud/external/api/get?token=${API_TOKEN}&v3`);
+        setGas(response.data)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 1000);
+
+    // Hủy interval khi component bị unmount
+    return () => clearInterval(intervalId);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2 className="main-header">Environmental parameters</h2>
+        <div className="meter">
+          <Temperature temperature={temp}/>
+          <GasMeter gasLevel={gas}/>
+          <HumidityMeter humidityLevel={humi}/>
+        </div> 
+        <div className="light-container">
+          <h2>Light control</h2>
+          <Button/>
+        </div> 
     </div>
   );
 }
